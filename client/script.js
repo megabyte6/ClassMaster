4e005bd7e04e1ac05dc99c8abf57edc114d182ad
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2023-10-30 19:52:24 -0700

    Add empty canvas for rendering game

--- client/script.js
+++ client/script.js
@@ -1,11 +1,3 @@
 const socket = new WebSocket(`ws://localhost:8080`);
 
-socket.onmessage = ({ data }) => {
-    console.log(`Message from server: ${data}`);
-};
-
-document.getElementById("send").addEventListener("click", () => {
-    const message = document.getElementById("message").value;
-    console.log("Sending message to server: " + message);
-    socket.send(message);
-});
+document.getElementById("join").addEventListener("click", () => {});
